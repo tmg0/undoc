e43f63f46fd956943ae531cc14772db4211c3144
zekunjin <zekun.jin@foxmail.com>
2023-02-15 16:33:26 +0800

    chore: code format

--- app.vue
+++ app.vue
@@ -6,20 +6,20 @@
 
 <style>
 ::-webkit-scrollbar {
-    height: 1rem;
-    width: 0.5rem;
+  height: 1rem;
+  width: 0.5rem;
 }
 
 ::-webkit-scrollbar-thumb {
-    --tw-border-opacity: 1;
-    background-color: rgba(217,217,227,.8);
-    border-color: rgba(255,255,255,var(--tw-border-opacity));
-    border-radius: 9999px;
-    border-width: 1px;
+  --tw-border-opacity: 1;
+  background-color: rgba(217,217,227,.8);
+  border-color: rgba(255,255,255,var(--tw-border-opacity));
+  border-radius: 9999px;
+  border-width: 1px;
 }
 
 ::-webkit-scrollbar-track {
-    background-color: transparent;
-    border-radius: 9999px;
+  background-color: transparent;
+  border-radius: 9999px;
 }
 </style>
