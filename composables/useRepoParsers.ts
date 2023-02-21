617b82836f498c0c7cceec9d05d0fdeecffbae75
zekunjin <zekun.jin@foxmail.com>
2023-02-21 11:48:52 +0800

    chore: use pathe parse filename

--- composables/useRepoParsers.ts
+++ composables/useRepoParsers.ts
@@ -1,14 +1,9 @@
+import { parse } from 'pathe'
 import mapKeys from 'lodash.mapkeys'
 
 const modules = import.meta.glob('~/utils/parsers/**/*.ts', { eager: true })
 
-const parseFilenameFromURL = (path: string) => {
-  const url = new URL(path, 'http:/127.0.0.1')
-  const [filename] = url.pathname.split('/').pop()?.split('.') || []
-  return filename
-}
-
-const parsers = mapKeys(modules, (_, key) => parseFilenameFromURL(key))
+const parsers = mapKeys(modules, (_, key) => parse(key).name)
 
 export const useRepoParsers = () => ({
   parsers (lib?: string, api?: string, version?: string) {
