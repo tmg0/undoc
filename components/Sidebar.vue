78a811c20a244ac45dda4e9eff02729832ad5735
zekunjin <zekun.jin@foxmail.com>
2023-02-23 15:37:05 +0800

    style: update sidebar item

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -4,20 +4,31 @@ import CaretRight from '~icons/carbon/caret-right'
 const emit = defineEmits(['select'])
 
 const store = useStore()
+const route = useRoute()
 
 const data = await $fetch('/api/used-apis')
 
 if (data) { store.cacheUsed(data) }
+
+const isSelected = (name: string, api?: string) => {
+  if (api) { return name === route.params.name && api === route.query.api }
+
+  return name === route.params.name
+}
 </script>
 
 <template>
-  <div class="flex flex-col h-full border-r border-gray-300/80" :style="{ width: '350px' }">
+  <div class="flex flex-col h-full border-r border-gray-300/80 select-none" :style="{ width: '350px' }">
     <NarBar />
 
     <div class="flex-1 px-4 py-3 overflow-y-auto">
       <details v-for="({ name, version, used }) in Object.values(store.libs)" :key="name">
-        <summary class="flex w-full justify-between cursor-pointer gap-1" @click="!used.length && emit('select', { lib: name })">
-          <div :title="name" class="flex items-center ">
+        <summary
+          class="flex w-full items-center justify-between cursor-pointer gap-1 hover:bg-gray-100 rounded pl-1 pt-1 pb-1 pr-3"
+          :class="{ 'bg-gray-100': !used.length && isSelected(name) }"
+          @click="!used.length && emit('select', { lib: name })"
+        >
+          <div :title="name" class="flex items-center">
             <div class="w-5">
               <CaretRight v-if="used.length" class="carbon-caret-right w-full flex-shrink-0 transition-all" />
             </div>
@@ -27,11 +38,19 @@ if (data) { store.cacheUsed(data) }
             </div>
           </div>
 
-          <div>{{ version }}</div>
+          <div class="bg text-sm text-gray-500/50">
+            {{ version }}
+          </div>
         </summary>
 
         <div class="pl-5">
-          <div v-for="item in used" :key="item" class="cursor-pointer" @click="emit('select', { lib: name, api: item })">
+          <div
+            v-for="item in used"
+            :key="item"
+            class="cursor-pointer hover:bg-gray-100 rounded px-3 py-1"
+            :class="{ 'bg-gray-100': isSelected(name, item) }"
+            @click="emit('select', { lib: name, api: item })"
+          >
             {{ item }}
           </div>
         </div>
