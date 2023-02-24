ddc6603d5783a6bc397217705c0792eb84914b2e
zekunjin <zekun.jin@foxmail.com>
2023-02-24 10:59:40 +0800

    feat: support custom undoc conf

--- server/api/node-config.get.ts
+++ server/api/node-config.get.ts
@@ -4,7 +4,7 @@ import { resolvePath } from 'mlly'
 export default defineEventHandler(async () => {
   try {
     const path = await resolvePath('./package.json', { url: process.cwd() })
-    const json = await fs.readFileSync(path).toString()
+    const json = fs.readFileSync(path).toString()
     return JSON.parse(json)
   } catch (error) { return { error } }
 })
--- server/api/undoc-config.get.ts
+++ server/api/undoc-config.get.ts
@@ -1,7 +1,12 @@
-import undocConf from '~~/undoc.config.json'
+import fs from 'node:fs'
+import { resolvePath } from 'mlly'
+import merge from 'lodash.merge'
+import defaultUndocConf from '~~/undoc.config.json'
 
-export default defineEventHandler(() => {
+export default defineEventHandler(async () => {
   try {
-    return undocConf as UndocConfig
+    const path = await resolvePath('./undoc.config.json', { url: process.cwd() })
+    const json = fs.readFileSync(path).toString()
+    return merge(defaultUndocConf, JSON.parse(json)) as UndocConfig
   } catch (error) { return { error } }
 })
